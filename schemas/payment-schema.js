899ac457c35809fd90a771107e79ed8fbbc02ac5
Rukshan Jayasekara <rukshanjayasekara@outlook.com>
2021-06-02 01:25:30 +0530

    added the one-to-many relationship between user and payments

--- schemas/payment-schema.js
+++ schemas/payment-schema.js
@@ -1,22 +1,13 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
 const paymentSchema = new mongoose.Schema({
-  payment_id: {
-    type: String,
-    required: [true, 'Payment ID is required']
-  },
-  amount: {
-    type: String,
-    required: [true, 'Amount is required']
-  },
-  paymentDate: {
-    type: String,
-    required: [true, 'Payment date is required']
-  },
-  userid: {
-    type: String,
-    required: [true, 'User ID is required']
-  }
+  payment_id: { type: String, required: [true, 'Payment ID is required'] },
+  amount: { type: String, required: [true, 'Amount is required'] },
+  paymentDate: { type: String, required: [true, 'Payment date is required'] },
+  userid: [{ type: mongoose.Types.ObjectId, required: true, ref: 'User' }]
 })
+
+paymentSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model('Payment', paymentSchema);
